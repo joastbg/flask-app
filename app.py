511e42e10982ea3ca7a50d702f6a4f975e7ad4eb
Johan Astborg <joastbg@gmail.com>
2016-07-31 14:32:45 +0200

    routing etc

--- app.py
+++ app.py
@@ -8,23 +8,29 @@ def home():
 
 @app.route("/videos")
 def videos():
-    return "Hello videos!"
+    #return "Hello videos!"
+	return render_template('index.html', headline="Videos")
+
 
 @app.route("/categories")
 def categories():
-    return "Hello categories!"
+    #return "Hello categories!"
+	return render_template('index.html', headline="Categories")
 	
 @app.route("/channels")
 def channels():
-    return "Hello channels!"
+    #return "Hello channels!"
+	return render_template('index.html', headline="Channels")
 	
-@app.route("/pornstars")
+@app.route("/actors")
 def pornstars():
-    return "Hello pornstars!"
+    #return "Hello actors!"
+	return render_template('index.html', headline="Actors")
 	
 @app.route("/search")
 def search():
-    return "Hello search!"
+    #return "Hello search!"
+	return render_template('index.html', headline="Search")
 
 if __name__ == "__main__":
 	app.run(debug=True)
