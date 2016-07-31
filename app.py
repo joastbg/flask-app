10c2d827cb8f195619e1e0e5c743b0c6a101a198
Johan Astborg <joastbg@gmail.com>
2016-07-31 15:16:50 +0200

    video dummies

--- app.py
+++ app.py
@@ -4,33 +4,42 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def home():
-    return render_template('index.html', headline="Welcome to my page!")
+    return render_template('index.html', headline="Most Recent Videos", page=1)
+	
+@app.route("/<page>")
+def home_page(page):
+    return render_template('index.html', headline="Most Recent Videos", page=int(float(page)))
 
 @app.route("/videos")
 def videos():
     #return "Hello videos!"
-	return render_template('index.html', headline="Videos")
+	return render_template('videos.html', headline="Videos")
+
+@app.route("/video/<id>")
+def video(id):
+    #return "Hello videos!"	
+	return render_template('video.html', headline="Videos", id=id)
 
 
 @app.route("/categories")
 def categories():
     #return "Hello categories!"
-	return render_template('index.html', headline="Categories")
+	return render_template('categories.html', headline="Categories")
 	
 @app.route("/channels")
 def channels():
     #return "Hello channels!"
-	return render_template('index.html', headline="Channels")
+	return render_template('channels.html', headline="Channels")
 	
 @app.route("/actors")
 def pornstars():
     #return "Hello actors!"
-	return render_template('index.html', headline="Actors")
+	return render_template('actors.html', headline="Actors")
 	
 @app.route("/search")
 def search():
     #return "Hello search!"
-	return render_template('index.html', headline="Search")
+	return render_template('search.html', headline="Search")
 
 if __name__ == "__main__":
 	app.run(debug=True)
